1b288e38593ba6cd8ffb99e91766e3b9830d3eaa
JLivlos <mistlivlos@gmail.com>
2022-11-27 21:40:37 +0300

    feat: complete arrays tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -455,8 +455,16 @@ function sortCitiesArray(arr) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  const arr = new Array(n);
+  arr.fill(new Array(n).fill(0));
+
+  const res = arr.map((item, index) => {
+    const newArr = item.slice();
+    newArr[index] = 1;
+    return newArr;
+  });
+  return res;
 }
 
 /**
@@ -547,8 +555,8 @@ function group(array, keySelector, valueSelector) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map((item) => childrenSelector(item)).flat();
 }
 
 
@@ -564,8 +572,11 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  if (!Array.isArray(arr[0])) {
+    return arr[indexes[0]];
+  }
+  return getElementByIndexes(arr[indexes[0]], indexes.splice(1));
 }
 
 
