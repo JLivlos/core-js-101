e6451efda54145968be6c3dde5d69e67c8e34bf9
JLivlos <mistlivlos@gmail.com>
2022-11-27 17:49:45 +0300

    feat: complete strings tasks

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -254,8 +254,24 @@ function getRectangleString(width, height) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  let res = '';
+  for (let i = 0; i < str.length; i += 1) {
+    const curCode = str[i].charCodeAt();
+    if (str[i] === ' ' || str[i] === '!' || str[i] === '?') {
+      res += str[i];
+    } else {
+      let newCode = curCode + 13;
+      if (curCode > 96 && newCode > 122) {
+        newCode -= 26;
+      }
+      if (curCode < 97 && newCode > 90) {
+        newCode -= 26;
+      }
+      res += String.fromCharCode(newCode);
+    }
+  }
+  return res;
 }
 
 /**
@@ -271,8 +287,13 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  try {
+    value.repeat();
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
 
 
@@ -300,8 +321,12 @@ function isString(/* value */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const deck = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+    'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+    'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠',
+  ];
+  return deck.indexOf(value);
 }
 
 
